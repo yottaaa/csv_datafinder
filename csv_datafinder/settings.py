b0bd0525cfabc436d2db8d7bc1aee3cd5c0df348
yottaaa <yotterick7@gmail.com>
2021-07-15 22:23:44 +0800

    Fix MEDIA_ROOT

--- csv_datafinder/settings.py
+++ csv_datafinder/settings.py
@@ -120,21 +120,14 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_URL = '/csv_files/'
+
 # Media files
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'csv_file')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# CLOUDINARY
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get("CLOUDINARY_NAME"),
-    'API_KEY': os.environ.get("CLOUDINARY_API_KEY"),
-    'API_SECRET': os.environ.get("CLOUDINARY_API_SECRET"),
-}
 
 # HEROKU config
 # django_heroku.settings(locals())
