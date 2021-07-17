e1918c7cca0ff85230ff69465dd6d35315a42ae6
yottaaa <yotterick7@gmail.com>
2021-07-17 13:09:20 +0800

    Add export file function

--- cdf_app/views.py
+++ cdf_app/views.py
@@ -1,4 +1,4 @@
-from os import sep
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 import pandas as pd
 import os
@@ -6,7 +6,7 @@ import secrets
 import json
 from .models import CSVData
 
-# Create your views here.
+# landing page
 def index(request):
     context = {}
     # if request method post
@@ -39,6 +39,7 @@ def index(request):
         context['files'] = csv_files
     return render(request, 'cdf_app/index.html', context)
 
+# detail view
 def dataView(request, pk):
     context = {}
     data = CSVData.objects.get(pk=pk)
@@ -47,11 +48,18 @@ def dataView(request, pk):
     df = df.applymap(str)
     # if searching data
     search_key = ''
+    search_col = ''
+    searchparam = {}
     if request.method == 'POST':
         search_col = request.POST.get('data_column')
         search_key = request.POST.get('data_search')
         if search_col != '' and search_key != '':
             df = df.loc[df[search_col].str.contains(search_key)] 
+        searchparam = {
+            'pk':pk,
+            'search_col':search_col,
+            'search_key':search_key,
+        }
         
     tb_rows = []
 
@@ -62,15 +70,52 @@ def dataView(request, pk):
             items.append(value)
         tb_rows.append(items)
 
+    print(searchparam)
+
     context = {
         'tb_header': df.columns, 
         'tb_rows': tb_rows, 
         'tb_shape': df.shape,
-        'search_key': search_key
+        'searchparam': searchparam,
+        'search_key': search_key,
     }
     return render(request, 'cdf_app/detail.html', context) 
 
+# for deleting data
 def dataDelete(request, pk):
     data = CSVData.objects.get(pk=pk)
     data.delete()
     return redirect('index')
+
+# for exporting data
+def exportData(request):
+    # load data
+    if request.GET:
+        data = CSVData.objects.get(pk=request.GET['pk'])
+        df = pd.read_json(data.data_json)
+
+        # convert to all types to strings
+        df = df.applymap(str)
+
+        # searching data
+        search_col = request.GET['search_col']
+        search_key = request.GET['search_key']
+        if search_col != '' and search_key != '':
+            df = df.loc[df[search_col].str.contains(search_key)]
+
+        response = HttpResponse(content_type='text/csv')
+
+        filename = ''
+        dataname = data.data_name
+        if search_key != '':
+            filename = dataname.replace('.csv', '_sortBy{}.csv'.format(search_key))
+        else:
+            filename = dataname
+
+        response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+
+        df.to_csv(response,index=False)
+
+        return response
+    else:
+        return HttpResponse(status=400)
